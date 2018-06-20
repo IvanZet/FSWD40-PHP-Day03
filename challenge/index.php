eb4b849e3192a3a0eeff532399a7d357cbb5add3
Ivan Zykov <ivan.v.zykov@gmail.com>
2018-06-20 09:45:43 +0200

    Adds initial files

---
+++ challenge/index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cool wheels</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  <a class="navbar-brand" href="index.php">Cool wheels</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+		  <div class="collapse navbar-collapse" id="navbarText">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="fleet.php">Our fleet<span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="reservation.php">Make reservation</a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Contacts</a>
+		      </li>
+		    </ul>
+		  </div>
+		</nav>
+	</header>
+	<main>
+		<div class="jumbotron">
+			<h1 class="display-4">Welcome to</h1>
+		  <h1>Cool wheels</h1>
+		  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
+	</main>
+</body>
+</html>
